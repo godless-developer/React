245bf6bb33524865ed64bcd33424dc70409a83aa
24LP6961 24LP6961 <24LP6961@G304-CC0231.local>
2024-12-17 11:11:35 +0800

    tseg

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import "./home.css";
+function Header() {
+  return <h1> Astronomy </h1>;
+}
+const App = () => {
+  return (
+    <div class="header">
+      <Header />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,5 @@
+import App from "./Home";
+import ReactDom from "react-dom/client";
+
+const root = ReactDom.createRoot(document.getElementById("root"));
+root.render(<App />);
